e9c5b49f477096cb09ad63ca2d69ce5e5bed769d
Mahmoud Mosalam <MahmoudMosalm076@gmail.com>
2024-03-02 16:20:32 +0200

    commit from mahmoud

--- lib/screens/learn_provider/riverpod.dart
+++ lib/screens/learn_provider/riverpod.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import 'model.dart';
-
+// hello
 final riverpodEasyLevel = StateProvider<int>((ref) {return 0;});
 final riverpodHardLevel = ChangeNotifierProvider<RiverpodModel>((ref) {
   return RiverpodModel(counter:0);
